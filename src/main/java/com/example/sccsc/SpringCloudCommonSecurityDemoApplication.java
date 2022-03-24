7aabf34ef1ed4eb5a0d9c4366a8a31fba3fb94eb
Corneil du Plessis <pcorneil@vmware.com>
2022-03-24 13:48:22 +0200

    Initial commit

---
+++ src/main/java/com/example/sccsc/SpringCloudCommonSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sccsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudCommonSecurityDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudCommonSecurityDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sccsc/SpringCloudCommonSecurityDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.sccsc;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudCommonSecurityDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
