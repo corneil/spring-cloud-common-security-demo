33839a8476a56a0e0c677541c4a4e080bb62dd64
Corneil du Plessis <pcorneil@vmware.com>
2022-03-24 13:48:38 +0200

    Reproduce spring-cloud-common-security-config cyclic dependency.

--- src/main/java/com/example/sccsc/SpringCloudCommonSecurityDemoApplication.java
+++ src/main/java/com/example/sccsc/SpringCloudCommonSecurityDemoApplication.java
@@ -1,9 +1,20 @@
 package com.example.sccsc;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        MetricsAutoConfiguration.class,
+        ManagementWebSecurityAutoConfiguration.class,
+        SecurityAutoConfiguration.class,
+        UserDetailsServiceAutoConfiguration.class,
+        SessionAutoConfiguration.class
+})
 public class SpringCloudCommonSecurityDemoApplication {
 
     public static void main(String[] args) {
